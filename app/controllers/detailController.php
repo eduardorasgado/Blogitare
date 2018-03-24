678e691cc98c9b8f73d91b05c53194a9bae7f016
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-03-24 03:04:20 -0600

    Se agrego un principio al paginado, se agrego link con nombre de articulo y id al url via detailController.php

--- app/controllers/detailController.php
+++ app/controllers/detailController.php
@@ -6,8 +6,26 @@ use App\Models\blogPost;
 
 class DetailController extends baseController {
 	public function getIndex($id){
+
+		//filtro para limpiar el $id
+		//ya que entra asi: 10_nombre_de_articulo
+		//Encontrando el comienzo de los _
+		$lenght = strlen($id);
+		$limit = 0;
+		for ($i=0;$i<$lenght;$i++){
+			if ($id[$i] == '_'){
+				$limit = $i-1	;
+				break;
+			}
+		}
+		//aislando $id desde el inicio hasta el limit
+		$new_id = '';
+		foreach(range(0,$limit) as $i){
+			$new_id = $new_id.$id[$i];
+		}
+		
 		//consiguiendo id de posts
-		$blogPosts = BlogPost::where('blog_id', $id)->get();
+		$blogPosts = BlogPost::where('blog_id', $new_id)->get();
 
 		return $this->render('detail.twig',[
 			'blogPosts' => $blogPosts
--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -7,13 +7,37 @@ use App\Models\blogPost;
 
 class IndexController extends baseController {
 
-	public function getIndex(){
+	public function getIndex($number = 1){
+		//paginacion del blog
+		$count = BlogPost::count();
+		$next = null;
+		$prev = null;
+
+		if($number > 1){
+			$prev = $number -1;
+		}
+		if($number < $count){
+			$next = $number +1;
+		}
+		$links = [
+			'next' => $next,
+			'prev' => $prev,
+		];
 
 		//la clase creada en blogPost.php de illuminate
 		//SELECT * FROM blog_posts  --->
-		$blogPosts = BlogPost::query()->orderBy('blog_id', 'desc')->get();
+		#$blogPosts = BlogPost::query()->orderBy('blog_id', 'desc')->get();
+
+		//Nueva query para paginacion
+		$blogPosts = BlogPost::query()->
+				orderBy('blog_id', 'desc')->
+					skip(2*($number-1))->
+							take(2)->get();
 
 		//la ruta ya no es completa puesto que ya la definimos dentro del constructor de la clase base
-		return $this->render('index.twig',['blogPosts' => $blogPosts]);
+		return $this->render('index.twig',[
+			'blogPosts' => $blogPosts,
+			'links' => $links
+		]);
 	}
 }
